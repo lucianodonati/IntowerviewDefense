3964d37ba27e66b754eacb6d5c52869acdc721ef
Luciano Donati <me@lucianodonati.com>
2020-09-25 20:06:53 -0300

    Added destroy script

---
+++ Assets/Scripts/Tools/DestroyAfterN.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Tools
+{
+    public class DestroyAfterN : MonoBehaviour
+    {
+        public float seconds = 1;
+
+        private IEnumerator Start()
+        {
+            yield return new WaitForSeconds(seconds);
+            Destroy(gameObject);
+        }
+    }
+}
