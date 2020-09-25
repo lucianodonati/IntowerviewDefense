ed9700531661c409647e0f26614c706b84479c72
Luciano Donati <me@lucianodonati.com>
2020-09-25 20:07:04 -0300

    Added inspector button attribute

---
+++ Assets/Scripts/Tools/InspectorButtonAttribute.cs
@@ -0,0 +1,43 @@
+﻿#if UNITY_EDITOR
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tools
+{
+    /// <summary>
+    /// Stick this on a method
+    /// </summary>
+    [AttributeUsage(AttributeTargets.All, AllowMultiple = true)]
+    public class EditorButtonAttribute : PropertyAttribute
+    {
+    }
+
+    [CustomEditor(typeof(MonoBehaviour), true)]
+    public class EditorButton : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            var mono = target as MonoBehaviour;
+
+            var methods = mono.GetType()
+                .GetMembers(BindingFlags.Instance | BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public |
+                            BindingFlags.NonPublic)
+                .Where(o => Attribute.IsDefined(o, typeof(EditorButtonAttribute)));
+
+            foreach (var memberInfo in methods)
+            {
+                if (GUILayout.Button(memberInfo.Name))
+                {
+                    var method = memberInfo as MethodInfo;
+                    method.Invoke(mono, null);
+                }
+            }
+        }
+    }
+#endif
+}
